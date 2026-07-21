f4bafad8f9b6c0bb89a8118917649aad841999b5
BeginnerBoi1 <santoschinkit@gmail.com>
2026-07-21 23:10:57 +0800

    Stage 4: full CRUD

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, status
 from pydantic import BaseModel
+from typing import Optional
+
 app = FastAPI()
 task_db = [
     {"id": 1, "title": "Buy groceries", "done":False},
@@ -10,6 +12,16 @@ task_db = [
 # for adding of task
 class TaskCreate(BaseModel):
     title: str
+
+# for the deletion/updating of tasks
+class _TaskUpdate(BaseModel):
+    title: Optional[str] = None
+    done: Optional[bool] = None
+
+class TaskResponse(BaseModel):
+    id: int
+    title: str
+    done: bool
 
 @app.get("/")
 def get_root():
@@ -58,3 +70,35 @@ def create_task(task : TaskCreate):
     task_db.append(new_task)
     return new_task
 
+# For deleting tasks
+@app.delete("/tasks/{task_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_task(task_id: int):
+    # Search for the task by its ID
+    for index, task in enumerate(task_db):
+        if task["id"] == task_id:
+            task_db.pop(index)  # Remove task
+            return 
+            
+    # Raise a 404 error if the task id does not exist
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND, 
+        detail=f"Task with ID {task_id} not found"
+    )
+
+# For updating tasks
+@app.put("/tasks/{task_id}", response_model=TaskResponse)
+def update_task(task_id: int, new: _TaskUpdate):
+    for task in task_db:
+        if task["id"] == task_id:
+            # Validate title if supplied
+            if new.title is not None:
+                if not new.title.strip():
+                    raise HTTPException(status_code=400, detail="Title cannot be empty.")
+                task["title"] = new.title.strip()
+            
+            if new.done is not None:
+                task["done"] = new.done
+                
+            return task
+            
+    raise HTTPException(status_code=404, detail=f"Task {task_id} not found")
